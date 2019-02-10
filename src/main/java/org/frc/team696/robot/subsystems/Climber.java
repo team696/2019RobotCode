65f8b868413909c80aab2efcba1cddc77661a883
Carlos Gross Jones <c.gross.jones@gmail.com>
2019-02-10 14:08:26 -0800

    Removing state machine

--- src/main/java/org/frc/team696/robot/subsystems/Climber.java
+++ src/main/java/org/frc/team696/robot/subsystems/Climber.java
@@ -11,7 +11,6 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.frc.team696.robot.OI;
 import org.frc.team696.robot.commands.ClimberManualControl;
-import org.frc.team696.robot.states.ClimberState;
 import org.frc.team696.robot.subsystems.ClimberModule;
 
 /**
@@ -22,7 +21,6 @@ public class Climber extends Subsystem {
   private static ClimberModule fr; 
   private static ClimberModule rl; 
   private static ClimberModule rr;
-  private static ClimberState state = ClimberState.IDLE;
   
   public static boolean isManual = false;
 
@@ -69,55 +67,10 @@ public class Climber extends Subsystem {
     setPower(power, power, power, power);
   }
 
-  public ClimberState getState(){
-    return state;
   }
 
   /**
-   * Setter method for climber subsystem state.
-   * This method contains transition checking;
-   * that is, if an illegal transition is requested, 
-   * it will not actually be implemented.
-   * @param newState The requested climber state
    */
-  public static void setState(ClimberState newState){
-    switch(state){
-      case IDLE:
-        switch(newState){
-          case IDLE:
-            break;
-          case MOVE_TO_ARMED:
-            state = newState;
-            break;
-          default:
-            //Other transitions not allowed
-            break;
-        }
-        break;
-      case MOVE_TO_ARMED:
-        switch(newState){
-          case MOVE_TO_STOWED:
-            state = newState;
-            break;
-          default:
-            //Other transitions not allowed
-            break;
-        }
-        break;
-      case ARMED:
-        switch(newState){
-          case CLIMBING:
-            state = newState;
-            break;
-          case MOVE_TO_STOWED:
-            state = newState;
-            break;
-          default:
-            //Other transitions not allowed
-            break;
-        }
-        break;
-    }
   }
 
   @Override
